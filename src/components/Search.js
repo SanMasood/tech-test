07f6f8d7b3ee0bed73db71fda3d8f432ddc63824
SanMasood <sanamasood.giki@gmail.com>
2020-06-24 17:16:36 +0100

    Returning parsed images & 2 image links in console

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,12 +8,18 @@ import logoImage from '../assets/Nasa-logo-3D.gif'
 
 const Search = () => {
     const [value, setValue] = useState('');
+
+    const handleSubmit= (event) => {
+        event.preventDefault();
+        getImages(value);
+    }
+
 return (
     
     <div className="Search">
         <img src={logoImage} data-testid="logo-id" className="nasa-logo" alt="NASA-logo"/>
 
-        <form className="input-form">Enter query: 
+        <form className="input-form" onSubmit={handleSubmit}>Enter query: 
         <input className="search-text" type="text" onChange={(e)=>setValue(e.target.value)}/>
 
         <button type="submit" className="search-button">Go!</button>
--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -8,7 +8,16 @@ const getImages = (query) => {
         return axios
         .get('https://images-api.nasa.gov/search?q=moon')
         .then((response)=>{
-            console.log(response)
+
+            let imageResults = response.data.collection.items;
+            console.log(imageResults);
+
+            let parsedImages = imageResults.filter((images)=> images.data[0].media_type==="image");
+
+            console.log("Parsed:" +parsedImages);
+
+            const images = parsedImages.map(image => image.links[0].href);
+            console.log (`Images: ${images}`);
         })
         .catch((err)=>{
             console.log(err)
