8b113fea6bef52b953494c18a4f02cda222b74c0
SanMasood <sanamasood.giki@gmail.com>
2020-07-01 19:00:13 +0100

    Updates

---
+++ precache-manifest.5e2a5f3529a88202de16fe1f9d785ed7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0212fb7a7af749402dbff34162bd9a",
+    "url": "/tech-test/index.html"
+  },
+  {
+    "revision": "5b669cf70f4163cefd2f",
+    "url": "/tech-test/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "c2655fd3ae6aaac5f732",
+    "url": "/tech-test/static/css/main.4f75b264.chunk.css"
+  },
+  {
+    "revision": "5b669cf70f4163cefd2f",
+    "url": "/tech-test/static/js/2.a99f6e44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tech-test/static/js/2.a99f6e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2655fd3ae6aaac5f732",
+    "url": "/tech-test/static/js/main.9e5abc2d.chunk.js"
+  },
+  {
+    "revision": "e53cdf7258c9f40463c1",
+    "url": "/tech-test/static/js/runtime-main.a73da4ab.js"
+  },
+  {
+    "revision": "c51d00eab610b7bb4316d4846497a3ad",
+    "url": "/tech-test/static/media/nasa-icon-png-7.c51d00ea.png"
+  }
+]);
